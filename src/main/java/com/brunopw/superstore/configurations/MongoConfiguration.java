0c6903e48c1a31ee30c039b98e60d5bfe299d71d
Bruno Warwzeniak <bruno_p_w@hotmail.com>
2020-06-10 11:57:23 +0100

    Mongo configurations

---
+++ src/main/java/com/brunopw/superstore/configurations/MongoConfiguration.java
@@ -0,0 +1,24 @@
+package com.brunopw.superstore.configurations;
+
+import com.brunopw.superstore.converters.ZonedDateTimeReadConverter;
+import com.brunopw.superstore.converters.ZonedDateTimeWriteConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.data.mongodb.core.convert.MongoCustomConversions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public @Configuration
+class MongoConfiguration {
+
+    private final List<Converter<?, ?>> converters = new ArrayList<>();
+
+    public @Bean
+    MongoCustomConversions customConversions() {
+        converters.add(new ZonedDateTimeReadConverter());
+        converters.add(new ZonedDateTimeWriteConverter());
+        return new MongoCustomConversions(converters);
+    }
+}
